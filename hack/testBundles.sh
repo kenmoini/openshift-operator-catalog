b9cd41a3775bec27248f4067c6b69163b4cc5944
Ken Moini <ken@kenmoini.com>
2025-09-03 01:34:38 -0400

    test separation

--- hack/testBundles.sh
+++ hack/testBundles.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
 
-# Check if the first arg is -q for quiet mode
-QUIET_MODE=false
-if [[ "$1" == "-q" ]]; then
-  QUIET_MODE=true
-fi
+# Fail on pull errors
+set -e
 
 # Check to see if yq is present
 if ! command -v yq &> /dev/null; then
-  if [ "$QUIET_MODE" =  false ]; then
-    echo "yq could not be found"
-  fi
+  echo "yq could not be found"
   exit 1
 fi
 
 # Get the script directory and go to the parent directory to be at the root of the project folder
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cd "$SCRIPT_DIR/.." || exit 1
+cd "$SCRIPT_DIR/.." || exit
 
 # Enable nullglob so that the pattern expands to nothing if no files are found
 shopt -s nullglob
-
-BUNDLE_IMAGES=""
 
 # Loop through each YML/YAML file in the bundles directory
 for file in bundles/*.{yml,yaml}; do
@@ -29,22 +22,10 @@ for file in bundles/*.{yml,yaml}; do
   if [[ -f "$file" ]]; then
     # Process the YAML file
     bundleName=$(yq -r '.name' "$file")
-    if [ "$QUIET_MODE" =  false ]; then
-      echo "Processing $file - $bundleName"
-    fi
+    echo "Processing $file - $bundleName"
     while IFS=$'\t' read -r img _; do
-      if [ "$QUIET_MODE" =  false ]; then
-        echo "- Bundle Image:  $img"
-      fi
-      BUNDLE_IMAGES+=",$img"
+      echo "- Bundle Image:  $img"
+      podman pull "$img"
     done < <(yq -r '.bundles[] | .image' "$file")
-    #
   fi
 done
-
-if [ "$QUIET_MODE" =  true ]; then
-  # Remove leading comma
-  BUNDLE_IMAGES=${BUNDLE_IMAGES:1}
-
-  echo "$BUNDLE_IMAGES"
-fi
